2eb3f5dbfe5f19fa21e7bfef608ff9002a4a0ecf
Chris Racklyeft <chris@theracklyefts.co.uk>
2022-10-22 20:44:48 +0100

    tidy up ctd

--- lib/widgets/ui/grid_card_default.dart
+++ lib/widgets/ui/grid_card_default.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_model/flutter_model.dart';
 
 class GridCardDefault<T extends IModel> extends StatelessWidget {
